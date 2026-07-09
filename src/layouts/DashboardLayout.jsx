d175f7efa0eaf6fb96f67feacfd90ef40f4509bc
ketanjangir129-stack <ketanjangir129@gmail.com>
2026-07-09 16:46:47 +0530

    updated layout page

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -14,7 +14,7 @@ function DashboardLayout() {
       <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
         <Header />
 
-        <main className="flex-1 overflow-y-auto p-4 sm:p-5 md:p-6">
+        <main className="flex-1 overflow-y-auto p-4 sm:p-5 md:p-6 hide-scrollbar">
           <Outlet />
         </main>
       </div>
