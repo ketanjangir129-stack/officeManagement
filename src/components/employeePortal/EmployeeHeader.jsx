a3ca67760249067635ad3f8f833938d193f65d69
ketanjangir129-stack <ketanjangir129@gmail.com>
2026-07-09 15:25:46 +0530

    updated files

--- src/components/employeePortal/EmployeeHeader.jsx
+++ src/components/employeePortal/EmployeeHeader.jsx
@@ -1,21 +1,50 @@
+import { useEmployees } from "../../context/EmployeeContext";
+
 function EmployeeHeader() {
+  const { employees } = useEmployees();
+  const employeeId = localStorage.getItem("employeeId");
+  const employee = employees.find(
+    (emp) => emp.id === employeeId
+  );
+
   return (
     <header className="bg-white px-4 md:px-6 py-4 border-b border-gray-200">
       <div className="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
-        {/* Search */}
-        <div className="relative w-full sm:max-w-md">
+
+        {/* Welcome */}
+        <div>
           <p className="text-lg font-semibold text-gray-900">
-            Welcome, to the oManagement
+            Welcome, {employee?.fullName || "Employee"}
+          </p>
+
+          <p className="text-sm text-gray-500">
+            Employee Portal
           </p>
         </div>
-        {/* Profile */}
-        <div className="flex items-end gap-3 self-end sm:self-auto">
-          <img
-            src="https://png.pngtree.com/png-clipart/20240917/original/pngtree-administrator-admin-avatar-png-image_16031562.png"
-            alt="Admin"
-            className="w-10 h-10 rounded-full object-cover"
-          />
+
+        {/* Avatar */}
+        <div className="flex items-center gap-3">
+
+          <div
+            className="
+              w-10
+              h-10
+              rounded-full
+              bg-violet-600
+              text-white
+              flex
+              items-center
+              justify-center
+              font-semibold
+              text-lg
+              uppercase
+            "
+          >
+            {employee?.fullName?.charAt(0) || "E"}
+          </div>
+
         </div>
+
       </div>
     </header>
   );
